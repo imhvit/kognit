5866b20f14a2c7e8d1711fc224ed0168e3f15acc
imhvit <yosifblancotafur2006@gmail.com>
2026-05-04 16:10:55 -0500

    feat: actualizar configuración de vite para incluir ssr y mejorar la gestión de plantillas de vue

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,31 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
-        inertia(),
+        inertia({ ssr: true }),
         tailwindcss(),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js"),
+        },
+    },
     server: {
         watch: {
             ignored: ["**/storage/framework/views/**"],
